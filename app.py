cbcf38555743b25f073c4436e80dd7592f7dfc2b
shkawan <shingo.ms@55mp.com>
2017-08-13 16:29:46 +0900

    remove a file and modify app.py

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 from bottle import route,run,request,response,redirect,default_app
 # import requests
 import os
+import datetime
 
 @route('/hello/:name')
 def hello(name):
   return "<h1>Hello {0}</h1>".format(name)
+
+@route('/datetime')
+def _datetime():
+  return datetime.datetime.now().strftime("%Y/%m/%dT%H:%M:%S")
 
 if __name__ == '__main__':
   port = os.environ.get("APP_PORT", 8080)
