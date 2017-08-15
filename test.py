75bee2c77eaf7df0dfea602c79bd39a5c320e2e1
shkawan <shingo.ms@55mp.com>
2017-08-15 23:28:01 +0900

    modify

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-from nose.tools import with_setup, raises, eq_
+from nose.tools import with_setup, raises, eq_, ok_
+
 def test_func001():
   eq_(1, 1)
-  
+
+def test_func002():
+  ok_("test func is ok")
+
